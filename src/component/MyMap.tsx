8866748d44e8714ab50939a2b02c04d81d5ffb87
JohnTing <jot.johnting@gmail.com>
2022-03-23 17:25:16 +0800

    zoom

--- src/component/MyMap.tsx
+++ src/component/MyMap.tsx
@@ -11,32 +11,33 @@ export default function MyMap() {
   const [position, setPosition] = useState<GeolocationPosition>()
   const [myMap, setMyMap] = useState<L.Map>()
   const [myMarker, setMyMarker] = useState<L.Marker>()
-  
 
-  useEffect(()=> {
+
+  useEffect(() => {
     const map = L.map('map').setView([24.1799, 120.6021], 64);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '<a href="https://www.openstreetmap.org/">OSM</a>',
-      maxZoom: 18,
-  }).addTo(map);
+      maxNativeZoom:19,
+      maxZoom: 20,
+    }).addTo(map);
 
-  setMyMap(map);
+    setMyMap(map);
 
-  
+
     const watchID = navigator.geolocation.watchPosition((position) => {
       setPosition(position)
 
       console.log(position.coords.latitude, position.coords.longitude);
     });
 
-    return () => {navigator.geolocation.clearWatch(watchID)};
+    return () => { navigator.geolocation.clearWatch(watchID) };
   }, [])
 
-  useEffect(()=> {
+  useEffect(() => {
 
     if (!position || !myMap)
       return;
-    
+
     const greenIcon = new L.Icon({
       iconUrl:
         "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
@@ -48,17 +49,37 @@ export default function MyMap() {
       shadowSize: [41, 41]
     });
 
-    if(myMarker) {
+    if (myMarker) {
       myMarker.remove()
     }
     const marker = L.marker([position.coords.latitude, position.coords.longitude], { icon: greenIcon }).addTo(myMap);
+
     setMyMarker(marker)
+
+
+
 
   }, [position])
 
 
+  function setview() {
+    if (myMap && position) {
+      myMap.setView([position.coords.latitude, position.coords.longitude])
+    }
+
+  }
+
+
 
   return (
-     <div id="map" style={{height: "100%", width: "100%"}}></div>
+    <div id="map" style={{ height: "100%", width: "100%" }}>
+
+      <div className="leaflet-bottom">
+        <input type="button" id="Btn2" value="Btn2" onClick={() => setview()} className="btnStyle span3 leaflet-control" />
+      </div>
+
+
+
+    </div>
   )
 }
